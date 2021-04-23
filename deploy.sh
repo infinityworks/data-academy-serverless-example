14c76ad4f5a957467b1010bbef46f3f7ef0d2308
Lewis Richardson <lewisjakerichardson@hotmail.com>
2021-04-23 14:51:51 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,17 +7,29 @@ logged_in=false
 
 for f in $FILES
 do
-  trimmed=${f: -46}
-  first_char=${trimmed:0:1}
+    # SSO token sessions are 46 characters long
+    # if the first_char from the trimmed filepath is a /
+    # then we know we have a session token
+    trimmed=${f: -46}
+    first_char=${trimmed:0:1}
 
-  if [ $first_char == '/' ]; then
-    logged_in=true
-    break
-  fi
+    if [ $first_char == '/' ]; then
+        sso_expiry=$(cat $f | jq -r '.expiresAt')
+        now=$(date +"%Y-%m-%dT%TZ")
+
+        # even with a session tokenm, it could still have expired
+        # if it has then log back in
+        if [[ "$sso_expiry" < "$now" ]]; then
+            aws sso login --profile $profile
+        fi
+            logged_in=true
+            break
+    fi
 done
 
+# no session token, log in
 if [ "$logged_in" = false ]; then
-  aws sso login --profile $profile        # login to AWS CLI
+    aws sso login --profile $profile
 fi
 
 npx ssocred $profile                    # generate temporary local credentials
